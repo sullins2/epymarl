a0a9d8d85108a96c91998dbd25f7355c5c40ce2c
sullins2 <35474542+sullins2@users.noreply.github.com>
2023-06-02 11:24:19 -0500

    Update q_learner.py

--- src/learners/q_learner.py
+++ src/learners/q_learner.py
@@ -82,7 +82,7 @@ class QLearner:
         target_mac_out = []
         self.target_mac.init_hidden(batch.batch_size)
         for t in range(batch.max_seq_length):
-            target_agent_outs = self.target_mac.forward(batch, t=t, nextobs=True)
+            target_agent_outs = self.target_mac.forward(batch, t=t, nextobs=False)
             target_mac_out.append(target_agent_outs)
         
 
