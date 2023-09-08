d9f920c3efc6b8eebe9424f125b3b89d6d3dc2e2
sullins2 <35474542+sullins2@users.noreply.github.com>
2023-09-08 12:10:33 -0500

    Update rnn_ns_agent.py

--- src/modules/agents/rnn_ns_agent.py
+++ src/modules/agents/rnn_ns_agent.py
@@ -19,14 +19,14 @@ class RNNNSAgent(nn.Module):
         qs = []
         if inputs.size(0) == self.n_agents:
             for i in range(self.n_agents):
-                q, h = self.agents[i](inputs[i].unsqueeze(0), hidden_state[:, i])
+                q, h = self.agents[i](inputs[i].unsqueeze(0), hidden_state[:, i], t_env=0)
                 hiddens.append(h)
                 qs.append(q)
             return th.cat(qs), th.cat(hiddens).unsqueeze(0)
         else:
             for i in range(self.n_agents):
                 inputs = inputs.view(-1, self.n_agents, self.input_shape)
-                q, h = self.agents[i](inputs[:, i], hidden_state[:, i])
+                q, h = self.agents[i](inputs[:, i], hidden_state[:, i], t_env=0)
                 hiddens.append(h.unsqueeze(1))
                 qs.append(q.unsqueeze(1))
             return th.cat(qs, dim=-1).view(-1, q.size(-1)), th.cat(hiddens, dim=1)
