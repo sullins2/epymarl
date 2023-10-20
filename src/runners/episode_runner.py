62c0b576bd62a3f1f204ff16590fc03c2e1c2a0c
sullins2 <35474542+sullins2@users.noreply.github.com>
2023-10-20 12:36:22 -0500

    Update episode_runner.py

--- src/runners/episode_runner.py
+++ src/runners/episode_runner.py
@@ -130,7 +130,7 @@ class EpisodeRunner:
 
             post_transition_data = {
                 "actions": actions,
-                "reward": [(rewards[0],rewards[1],rewards[2],rewards[3])], #[(reward,)], #[(reward,)],
+                "reward": [(reward,)], #[(rewards[0],rewards[1],rewards[2],rewards[3])], #[(reward,)], # qtran_base
                 "terminated": [(terminated != env_info.get("episode_limit", False),)],
                 # "nextobs": [self.env.get_obs()]
             }
@@ -187,18 +187,18 @@ class EpisodeRunner:
         self.ret += [result1]
 
         # Applying reward shaping
-        totalRew = sum(cumRew) + 0.1 #0.1 is from original paper
-        for i in range(4):
-          for t in range(self.t):
-            curRew[i][t] += (40.0 / 3.0)*(totalRew - cumRew[i]) / self.t
+        # totalRew = sum(cumRew) + 0.1 #0.1 is from original paper
+        # for i in range(4):
+        #   for t in range(self.t):
+        #     curRew[i][t] += (40.0 / 3.0)*(totalRew - cumRew[i]) / self.t
 
         # set them all as curRew
-        for t in range(self.t):
-          # temp = 0.0
-          for i in range(4):
-            # temp += curRew[i][t]
+        # for t in range(self.t):
+        #   # temp = 0.0
+        #   for i in range(4):
+        #     # temp += curRew[i][t]
               
-            self.batch.data.transition_data["reward"][0][t][i] = curRew[i][t]
+        #     self.batch.data.transition_data["reward"][0][t][i] = curRew[i][t]
           # self.batch.data.transition_data["reward"][0][t] = temp
 
 
